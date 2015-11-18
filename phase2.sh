19b65ef539b43abb5c4e3b522a8853bc24d371fa
tristan-hunt <thunt@ualberta.ca>
2015-11-18 14:45:18 -0700

    Update and rename pipeline.txt to phase2.sh

--- phase2.sh
+++ phase2.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+# These are the commands that have to be run for phase2:
 sort -u reviews.txt | perl break.pl -e >| output.txt
 db_load -T -t hash -f output.txt rw.idx
 sort -u pterms.txt | perl break.pl -e >| output.txt
